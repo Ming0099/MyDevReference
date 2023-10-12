b8dae636a584eb654f79d4bee3f208b6b70fd337
Ming0099 <130200978+Ming0099@users.noreply.github.com>
2023-10-12 21:54:22 +0900

    Update 특수문자.js
    JavaScript 특수문자 추가

--- JavaScript/리터럴과 데이터/특수문자.js
+++ JavaScript/리터럴과 데이터/특수문자.js
@@ -14,3 +14,37 @@ const C12 = "\b"; // 백스페이스
 const C13 = "\f"; // 폼 피드
 
 console.log("[" + C13 + "]"); // Cn
+
+
+/* 템플릿 문자열 */
+let currentTemp = 19.5;
+const message = "The current temperature is " + currentTemp + "\u00b0C";
+console.log(message);
+
+/* 문자열 템플릿에는 큰따옴표나 작은따옴표가 아닌 백틱(`)을 사용한다. */
+let currentTemp2 = 19.5;
+const message2 = `The current temperature is ${currentTemp2}\u00b0C`;
+console.log(message2);
+
+
+/* 여러줄 문자열 */
+const multiline = "line1\
+line2"; 
+//line1line2
+
+const multiline2 = "line1\n\
+line2"
+//line1
+//line2
+
+const multiline3 = `line1
+line2`
+//line1
+//line2
+
+console.log(multiline3);
+
+
+/* 숫자와 문자열 */
+const result1 = 3 + '30'; // 3이 문자열로 바뀐다. ('330')
+const result2 = 3 * '30'; // '30'이 숫자로 바뀐다. (90)
